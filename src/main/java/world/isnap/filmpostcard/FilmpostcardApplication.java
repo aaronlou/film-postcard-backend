9367954c1fd8acbd0bd711efd96767407aec0640
lou_.5@aliyun.com <siyuanlou1986@petalmail.com>
2025-11-14 09:34:29 +0800

    init backend project

---
+++ src/main/java/world/isnap/filmpostcard/FilmpostcardApplication.java
@@ -0,0 +1,13 @@
+package world.isnap.filmpostcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmpostcardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmpostcardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/world/isnap/filmpostcard/FilmpostcardApplicationTests.java
@@ -0,0 +1,13 @@
+package world.isnap.filmpostcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmpostcardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
